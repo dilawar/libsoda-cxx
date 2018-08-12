6bd44f6396d5c3b17a75e0b842af219cc0839c90
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-13 03:00:55 +0530

    Enabled more tests.

--- test_LSODA.cpp
+++ test_LSODA.cpp
@@ -186,8 +186,6 @@ int main(int argc, const char *argv[])
     chrono::steady_clock::time_point end= chrono::steady_clock::now();
     cout << "|| Time taken (us)= " << chrono::duration_cast<chrono::microseconds>(end - begin).count() <<endl;
 
-#if 0
-
     begin = chrono::steady_clock::now();
     test_fex();
     end = chrono::steady_clock::now();
@@ -200,7 +198,6 @@ int main(int argc, const char *argv[])
     cout << "|| Time taken (us)="
         << chrono::duration_cast<chrono::nanoseconds> (end - begin).count()/1000
         <<endl;
-#endif
 
     return 0;
 }
