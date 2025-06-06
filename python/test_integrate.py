3c1df81ac93d3597aa31e703f2c40d0d7f322c5d
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-06-06 12:20:06 +0530

    run python script using uv

--- python/test_integrate.py
+++ python/test_integrate.py
@@ -1,19 +1,28 @@
-from pylab import *
+# Run: `uv run test_integrate.py`
+#
+# /// script
+# dependencies = [
+#    'numpy',
+#    'scipy',
+#  ]
+# ///
+
+import numpy as np
 from scipy.integrate import ode
 import time
     
 mu = 1000
-y0 = r_[0.5, 0.5]
+y0 = np.r_[0.5, 0.5]
 T = 500
-tt = linspace(T/200,T, 100)
+tt = np.linspace(T/200,T, 100)
 
 def van_der_pol(t, y):
-    return r_[y[1], mu*(1.0-y[0]**2)*y[1]-y[0]]
+    return np.r_[y[1], mu*(1.0-y[0]**2)*y[1]-y[0]]
 
 c2 = time.process_time()
 r2 = ode(van_der_pol).set_integrator('lsoda')
 r2.set_initial_value(y0)
-sol2 = array([r2.integrate(T) for T in tt])
+sol2 = np.array([r2.integrate(T) for T in tt])
 
 for t, y in zip(tt, sol2[:,0]):
     print(t, y)
